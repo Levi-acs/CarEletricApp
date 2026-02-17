e877525627100a216e90ed55a5aeeb60dcc2c8e9
LeviNascimento07 <levidonascimento2410@gmail.com>
2026-02-17 14:06:45 -0300

    formatando lista de favoritos, e terminando o projeto

--- app/src/main/java/com/example/careletricapp/presetantion/FavoritosFragment.kt
+++ app/src/main/java/com/example/careletricapp/presetantion/FavoritosFragment.kt
@@ -1,16 +1,21 @@
 package com.example.careletricapp.presetantion
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import androidx.core.view.isVisible
 import com.example.careletricapp.R
+import com.example.careletricapp.domain.Carro
 import com.example.careletricapp.local.CarRepository
+import com.example.careletricapp.presetantion.adapter.CarAdapter
 
 
 class FavoritosFragment : Fragment() {
+
+    lateinit var listaCarrosFavoritos: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,9 +29,32 @@ class FavoritosFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setupView(view)
+        setupList()
+    }
+
+    private fun getCarsOnLocalDb(): List<Carro> {
         val repository = CarRepository(requireContext())
         val carrList = repository.getAll()
+        return carrList
+    }
 
-        Log.d("Lista da carros", carrList.size.toString())
+    fun setupView(view: View) {
+        view.apply {
+            listaCarrosFavoritos = findViewById(R.id.rv_lista_carros_favoritos)
+        }
+
+    }
+
+    fun setupList() {
+        val cars = getCarsOnLocalDb()
+        val carroAdapter = CarAdapter(cars)
+        listaCarrosFavoritos.apply {
+            isVisible = true
+            adapter = carroAdapter
+        }
+        carroAdapter.carItemLister = { carro ->
+            // val isSaved = CarRepository(requireContext()).saveIfNotExist(carro)
+        }
     }
 }
